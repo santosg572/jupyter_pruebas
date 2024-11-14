63f54732172ae31f229a5cfcd5ed6f5614bfa78f
santosg572 <santosg572@gmail.com>
2024-11-14 05:33:41 -0600

    Test comité

--- levanta_jupyter.sh
+++ levanta_jupyter.sh
@@ -2,6 +2,7 @@
 
 #jupyter lab --notebook-dir=/Users/santosg
 
-jupyter lab --notebook-dir=/Users/leopoldogonzalez
+#jupyter lab --notebook-dir=/Users/leopoldogonzalez
 
+jupyter lab --notebook-dir=$PWD
 
